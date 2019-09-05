04d6069cde7fdada659d135ce53def3e3a98f123
ShaynAli <shayaan.syed.ali@gmail.com>
2019-09-05 03:50:48 -0400

    WIP state - issues with __get_attribute__

--- cached_hash.py
+++ cached_hash.py
@@ -6,18 +6,37 @@ This allows for the efficient use of dicts in cases when subclasses are rarely m
 
 class CachedHash(object):
 
+    default_safe_names = frozenset({
+        '__hash'
+    })
+
     def _subclass(self):
         return self.__class__.mro()[0]
 
-    def __getattribute__(self, item):
+    def __getattribute__(self, item):  # TODO: Should be a set function/should we add checks to builtin set functions?
+        breakpoint()  # TODO: Fix (WIP)
+        if item == 'safe_names':
+            return vars(self)['safe_names']
         if item not in self.safe_names:
             self.__hash = None
         return self._subclass().__getattribute__(item)
 
-    def __init__(self, safe_names=frozenset()):  # TODO: Identify good names to expect to be safe from mutations
+    def __init__(self, safe_names=default_safe_names):  # TODO: Identify good names to expect to be safe from mutations
         self.__hash = None
         self.safe_names = safe_names
 
     def __hash__(self):
         if self.__hash is not None:
             return self.__hash
+
+
+class Example(CachedHash):
+
+    def __init__(self, a=None, b=10):
+        super().__init__()
+        self.a = a
+        self.b = b
+
+    def reset(self):
+        self.a = None
+        self.b = 10
